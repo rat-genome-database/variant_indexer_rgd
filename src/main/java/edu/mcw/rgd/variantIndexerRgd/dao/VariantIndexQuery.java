40a189c339997940afc6a556eb0e319eb821427e
Thota <jthota@RGD-SHIM-MAC.local>
2022-01-18 09:41:51 -0600

    fixed no transcript variants

--- src/main/java/edu/mcw/rgd/variantIndexerRgd/dao/VariantIndexQuery.java
+++ src/main/java/edu/mcw/rgd/variantIndexerRgd/dao/VariantIndexQuery.java
@@ -66,14 +66,17 @@ public class VariantIndexQuery extends MappingSqlQuery {
                     vi.setVariantTranscripts(vts);
             }}catch (Exception e){}
             /******************region_name*******************/
-            String regionName=rs.getString("region_name");
-            List<String> regionNames=new ArrayList<>();
-            if(regionName!=null) {
-                regionNames.add(regionName);
-                vi.setRegionName(regionNames);
+            try {
+                    String regionName = rs.getString("region_name");
+                    List<String> regionNames = new ArrayList<>();
+                    if (regionName != null) {
+                            regionNames.add(regionName);
+                            vi.setRegionName(regionNames);
 
-                vi.setRegionNameLc(Arrays.asList(regionName.toLowerCase()));
-            };
+                            vi.setRegionNameLc(Arrays.asList(regionName.toLowerCase()));
+                    }
+                    ;
+            }catch (Exception e){}
             try {
                     if(rs.getInt("gene_rgd_id")>0) {
                             vi.setGeneRgdId(rs.getInt("gene_rgd_id"));
