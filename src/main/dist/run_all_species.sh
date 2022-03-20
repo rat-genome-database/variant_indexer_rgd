af3e4889649c60b90715fd0654c89df8e365dab4
Thota <jthota@rgd-shim-mac.lan>
2022-03-19 22:19:27 -0500

    updated scripts

--- src/main/dist/run_all_species.sh
+++ src/main/dist/run_all_species.sh
@@ -1,20 +1,20 @@
 APPDIR=/home/rgddata/pipelines/variant_indexer_rgd
 
 ENV=$1
-$APPDIR/rat_all_chr.sh $ENV 3 372
-$APPDIR/rat_all_chr.sh $ENV 3 360
-$APPDIR/rat_all_chr.sh $ENV 3 60
-$APPDIR/rat_all_chr.sh $ENV 3 70
+$APPDIR/rat_all_chr.sh $ENV 372 3
+$APPDIR/rat_all_chr.sh $ENV 360 3
+$APPDIR/rat_all_chr.sh $ENV 60 3
+$APPDIR/rat_all_chr.sh $ENV 70 3
 
-$APPDIR/dog_all_chr.sh $ENV 6 631
+$APPDIR/dog_all_chr.sh $ENV 631 6
 
-$APPDIR/pig_all_chr.sh $ENV 9 910
-$APPDIR/pig_all_chr.sh $ENV 9 911
+$APPDIR/pig_all_chr.sh $ENV 910 9
+$APPDIR/pig_all_chr.sh $ENV 911 9
 
-$APPDIR/greenMonkey_all_chr.sh $ENV 13 1311
+$APPDIR/greenMonkey_all_chr.sh $ENV 1311 13
 
-$APPDIR/mouse_all_chr.sh $ENV 2 35
+$APPDIR/mouse_all_chr.sh $ENV 35 2
 
-$APPDIR/human_all_chr.sh $ENV 1 38
-$APPDIR/human_all_chr.sh $ENV 1 17
+$APPDIR/human_all_chr.sh $ENV 38 1
+$APPDIR/human_all_chr.sh $ENV 17 1
 
